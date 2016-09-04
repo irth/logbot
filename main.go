16c9c483e6bfcc559ea6944a5f3cc72924b48c2a
Wojciech Kwolek <me@irth.pl>
2016-09-04 21:21:46 +0200

    Log messages to files.

--- main.go
+++ main.go
@@ -1,18 +1,64 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
 	"github.com/thoj/go-ircevent"
+	"os"
+	"path"
+	"text/template"
 	"time"
 )
+
+type FilenameData struct {
+	Year  string
+	Month string
+	Day   string
+}
+
+func Write(filenameTemplate string, text string) {
+	var t = time.Now()
+	var d = FilenameData{
+		Year:  t.Format("2006"),
+		Month: t.Format("01"),
+		Day:   t.Format("02"),
+	}
+	var tmpl, err = template.New("filename").Parse(filenameTemplate)
+	if err != nil {
+		panic(err)
+	}
+
+	var filenameBytes bytes.Buffer
+	err = tmpl.Execute(&filenameBytes, d)
+	if err != nil {
+		panic(err)
+	}
+	var filename = filenameBytes.String()
+
+	err = os.MkdirAll(path.Dir(filename), 0777)
+	if err != nil {
+		panic(err)
+	}
+
+	file, err := os.OpenFile(filename, os.O_APPEND|os.O_RDWR|os.O_CREATE, 0666)
+	defer file.Close()
+	if err != nil {
+		panic(err)
+	}
+
+	_, err = file.WriteString(text + "\n")
+	if err != nil {
+		panic(err)
+	}
+}
 
 func main() {
 	var addr = flag.String("addr", "chat.freenode.net:6667", "the irc server address")
 	var nick = flag.String("nick", "[logbot]", "bot's nickname")
 	var ident = flag.String("ident", "logbot", "bot's username")
 	var channel = flag.String("channel", "#channel", "channel to join")
-	//var filename = flag.String("filename", "/var/www/logs/{{.Year}}/{{.Year}}-{{.Month}}-{{.Day}}.txt", "where the logs should be stored")
+	var filename = flag.String("filename", "/var/www/logs/{{.Year}}/{{.Year}}-{{.Month}}-{{.Day}}.txt", "where the logs should be stored")
 	flag.Parse()
 
 	// Connect to IRC
@@ -24,13 +70,13 @@ func main() {
 	conn.AddCallback("PRIVMSG", func(e *irc.Event) {
 		var timestamp = time.Now().Format("15:04:05")
 		var msg = fmt.Sprintf("%s <%s> %s", timestamp, e.Nick, e.Message())
-		fmt.Println(msg)
+		Write(*filename, msg)
 	})
 
 	conn.AddCallback("CTCP_ACTION", func(e *irc.Event) {
 		var timestamp = time.Now().Format("15:04:05")
 		var msg = fmt.Sprintf("%s * %s %s", timestamp, e.Nick, e.Message())
-		fmt.Println(msg)
+		Write(*filename, msg)
 	})
 
 	conn.Wait()
