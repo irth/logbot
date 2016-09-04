9a345fa2ac5015946bf7861876f2e358aeea2d2d
Wojciech Kwolek <me@irth.pl>
2016-09-04 14:25:28 +0200

    Implement connecting to IRC

--- main.go
+++ main.go
@@ -2,17 +2,22 @@ package main
 
 import (
 	"flag"
-	"fmt"
-	_ "github.com/thoj/go-ircevent"
+	"github.com/thoj/go-ircevent"
 )
 
 func main() {
 	var addr = flag.String("addr", "chat.freenode.net:6667", "the irc server address")
 	var nick = flag.String("nick", "[logbot]", "bot's nickname")
 	var ident = flag.String("ident", "logbot", "bot's username")
-	var realname = flag.String("realname", "beep boop imma bot", "bot's realname")
 	var channel = flag.String("channel", "#channel", "channel to join")
-	var filename = flag.String("filename", "/var/www/logs/{{.Year}}/{{.Year}}-{{.Month}}-{{.Day}}.txt", "where the logs should be stored")
+	//var filename = flag.String("filename", "/var/www/logs/{{.Year}}/{{.Year}}-{{.Month}}-{{.Day}}.txt", "where the logs should be stored")
 	flag.Parse()
-	fmt.Printf("%s, %s, %s, %s, %s, %s\n", *addr, *nick, *ident, *realname, *channel, *filename)
+
+	// Connect to IRC
+	conn := irc.IRC(*nick, *ident)
+
+	conn.Connect(*addr)
+	conn.Join(*channel)
+
+	conn.Wait()
 }
